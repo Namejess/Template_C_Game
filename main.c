31544e17c9faeac672138765cdfcf987b20e5868
Jessy DROUIN <jessy.drouin@equasens.com>
2023-04-19 21:53:31 +0200

    Background

--- main.c
+++ main.c
@@ -4,10 +4,51 @@
 #include <SDL2/SDL.h>
 #include "SDL2/SDL_image.h"
 
-#define SDL_ASSERT_LEVEL 2
 #define kGameName "SDL2 Game"
 #define kGameWidth 800
 #define kGameHeight 600
+
+SDL_Texture *LoadTexture(SDL_Renderer *renderer, char *path)
+{
+    SDL_Texture *texture = NULL;
+    SDL_Surface *loadedSurface = IMG_Load(path);
+
+    if (loadedSurface == NULL)
+    {
+        printf("Unable to load image %s! SDL_image Error: %s", path, IMG_GetError());
+        return NULL;
+    }
+    else
+    {
+        texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        if (texture == NULL)
+        {
+            printf("Unable to create texture from %s! SDL Error: %s", path, SDL_GetError());
+            return NULL;
+        }
+        else
+        {
+            SDL_FreeSurface(loadedSurface);
+            loadedSurface = NULL;
+        }
+
+        return texture;
+    }
+}
+
+SDL_Texture *LoadTexture2(SDL_Renderer *renderer, char *path)
+{
+    SDL_Texture *texture = NULL;
+
+    texture = IMG_LoadTexture(renderer, path);
+    if (texture == NULL)
+    {
+        printf("Unable to create texture from %s! SDL Error: %s", path, SDL_GetError());
+        return NULL;
+    }
+
+    return texture;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -47,32 +88,114 @@ int main(int argc, char const *argv[])
         printf("SDL_CreateRenderer Error\n%s", SDL_GetError());
         return -1;
     }
-
-    // Main Game loop
-    while (true)
+    else
     {
-        // Handle events
-        SDL_Event event;
-        SDL_PollEvent(&event);
-
-        switch (event.type)
+        int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+        if (!IMG_Init(imgFlags) & imgFlags)
         {
-        case SDL_QUIT:
-            return 0;
-            break;
+            printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+            return -1;
         }
 
-        // Clear the screen
-        SDL_RenderClear(renderer);
+        SDL_Texture *texBG = LoadTexture(renderer, "gfx/background.jpg");
+        int iTexBG_w, iTexBG_h;
+        SDL_QueryTexture(texBG, NULL, NULL, &iTexBG_w, &iTexBG_h);
 
-        // Draw the screen
-        SDL_RenderPresent(renderer);
+        SDL_Texture *texPlanet = LoadTexture2(renderer, "gfx/planet.png");
+        int iTexPlanet_w, iTexPlanet_h;
+        SDL_QueryTexture(texPlanet, NULL, NULL, &iTexPlanet_w, &iTexPlanet_h);
+        int x = 0;
+        int y = 0;
+        int vx = 4;
+        int vy = 4;
+
+        int xBG = 0;
+
+        // Main Game loop
+        while (true)
+        {
+            // Handle events
+            SDL_Event event;
+            SDL_PollEvent(&event);
+
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                return 0;
+                break;
+            }
+            // Update
+            x += vx;
+            y += vy;
+
+            xBG -= 4;
+
+            if (xBG <= -iTexBG_w)
+            {
+                xBG = 0;
+            }
+
+            if (x + iTexPlanet_w > kGameWidth)
+            {
+                vx = -vx;
+                x = kGameWidth - iTexPlanet_w;
+            }
+            if (x < 0)
+            {
+                vx = -vx;
+                x = 0;
+            }
+
+            if (y + iTexPlanet_h > kGameHeight)
+            {
+                vy = -vy;
+                y = kGameHeight - iTexPlanet_h;
+            }
+            if (y < 0)
+            {
+                vy = -vy;
+                y = 0;
+            }
+
+            // Clear the screen
+            SDL_RenderClear(renderer);
+
+            // Draw a rectangle white
+            SDL_Rect rect;
+            rect.x = x;
+            rect.y = y;
+            rect.w = iTexPlanet_w;
+            rect.h = iTexPlanet_h;
+
+            SDL_Rect rectBG;
+            rectBG.x = xBG;
+            rectBG.y = 0;
+            rectBG.w = iTexBG_w;
+            rectBG.h = iTexBG_h;
+            // Affichage background BG1 et BG2
+            SDL_RenderCopy(renderer, texBG, NULL, &rectBG);
+
+            SDL_Rect rectBG2;
+            rectBG.x = xBG + iTexBG_w;
+            rectBG.y = 0;
+            rectBG.w = iTexBG_w;
+            rectBG.h = iTexBG_h;
+            SDL_RenderCopy(renderer, texBG, NULL, &rectBG2);
+
+            // Affichage image
+            SDL_RenderCopy(renderer, texPlanet, NULL, &rect);
+
+            // Draw the screen
+            SDL_RenderPresent(renderer);
+        }
+
+        // Clean up
+        SDL_DestroyTexture(texPlanet);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        SDL_Quit();
+
+        return 0;
     }
-
-    // Clean up
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
 }
